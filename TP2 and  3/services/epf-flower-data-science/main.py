3c10390b96a06255eeada1d4b87a5225c550bce2
BDaipa <blandine.daipa@epfedu.fr>
2024-11-26 15:50:54 +0100

    Initial API setup

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,8 +1,14 @@
 import uvicorn
+from fastapi.responses import RedirectResponse
 
 from src.app import get_application
 
 app = get_application()
 
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
+
